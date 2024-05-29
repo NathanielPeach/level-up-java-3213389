1b74399bc1d2422d4c7d1e33d06bcc1ef18ea401
Nathaniel Peach <101123862+NathanielPeach@users.noreply.github.com>
2024-05-29 21:13:49 +0000

    Creation of a simple person object.

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -2,8 +2,17 @@ package com.linkedin.javacodechallenges;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        
+    public static void main( String[] args ) {
+        Person person = new Person();
+        person.setFirstName("John");
+        person.setLastName("Doe");
+        person.setAge(42);
+
+        greetings(person);
+    }
+
+    public static void greetings(Person human) {
+        System.out.println("Greetings " + human.getFirstName() + " " + human.getLastName() + 
+        ". You are " + human.getAge() + " years old.");
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,31 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public void setFirstName(String name) {
+    this.firstName = name;
+  }
+
+  public void setLastName(String name) {
+    this.lastName = name;
+  }
+
+  public void setAge(int currentage) {
+    this.age = currentage;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
 }
